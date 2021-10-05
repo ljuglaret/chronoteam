a010c0068c01e8b6297c1ce34a2b84758d1e697c
ljuglaret <ljuglaret@gmail.com>
2021-10-05 15:24:15 +0200

    Updates

--- src/LoginPage.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import {FormPerson} from './LoginPage';
-
-test('renders learn react link', () => {
-  render(<FormPerson />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
